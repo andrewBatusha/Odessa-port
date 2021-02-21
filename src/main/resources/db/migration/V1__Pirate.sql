fdeae53fb33193e2381a96445213e642411f31ff
andrewBatusha <andrew.batuiev@gmail.com>
2021-02-21 17:19:51 +0200

    add sql script

---
+++ src/main/resources/db/migration/V1__Pirate.sql
@@ -0,0 +1,74 @@
+create table crews
+(
+    id serial not null
+        constraint crews_pkey
+            primary key,
+    captain varchar(255),
+    nationality varchar(255),
+    size integer not null
+);
+
+alter table crews owner to lpwptzohkkcavy;
+
+create table orders
+(
+    id serial not null
+        constraint orders_pkey
+            primary key,
+    client varchar(255),
+    description varchar(255),
+    order_status varchar(255),
+    reward integer not null,
+    crew_id serial
+        constraint fkoqlc23wl8bviqgumlyy4hsjik
+            references crews
+);
+
+alter table orders owner to lpwptzohkkcavy;
+
+create table ships
+(
+    id serial not null
+        constraint ships_pkey
+            primary key,
+    name varchar(255),
+    ship_type varchar(255),
+    speed integer not null,
+    crew_id serial
+        constraint fkobrhdshhfq0ugoo9vumrmgsiv
+            references crews
+);
+
+alter table ships owner to lpwptzohkkcavy;
+
+create table hold_items
+(
+    id serial not null
+        constraint hold_items_pkey
+            primary key,
+    hold_type varchar(255),
+    name varchar(255),
+    quantity integer not null,
+    ship_id serial
+        constraint fkic45fxwcgta0hbld8c8est27l
+            references ships
+);
+
+alter table hold_items owner to lpwptzohkkcavy;
+
+create table weapons
+(
+    id serial not null
+        constraint weapons_pkey
+            primary key,
+    name varchar(255),
+    quantity integer not null,
+    weapons_type varchar(255),
+    wear varchar(255),
+    ship_id serial
+        constraint fkp837wr183pba3855q399btyvl
+            references ships
+);
+
+alter table weapons owner to lpwptzohkkcavy;
+
